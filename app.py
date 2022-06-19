7c98ae9bfcd981f8a53dc90129691cfd0f01e9d9
Simon Tran <simtran17@gmail.com>
2022-06-18 21:43:56 -0400

    Add Get Started button and second page

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/type")
+def type():
+    return render_template("type.html")
 
 
 if __name__ == "__main__":
--- static/app.js
+++ static/app.js
@@ -1,1 +1,3 @@
-
+document.getElementById("get-started").onclick = function () {
+    location.href = "/type"
+}
