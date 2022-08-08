5cdb950a7e897167569c14d6d80884e8294f8536
Simon Tran <simontran@simons-mbp.lan>
2022-08-08 14:22:37 -0400

    Setup Google Maps Places API

--- scraper.py
+++ scraper.py
@@ -2,5 +2,13 @@ from dataclasses import dataclass
 from xml.dom.pulldom import SAX2DOM
 
 
+import googlemaps
+from dotenv import load_dotenv
+
+load_dotenv()
+
+gmaps = googlemaps.Client(key="API_KEY")
+
+
 def search(restaurant):
     print("searching " + restaurant)
