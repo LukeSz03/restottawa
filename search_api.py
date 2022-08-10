f95feb3685a38bfb3eaf24eaa17219dd3cb9de9b
Simon Tran <simontran33@gmail.com>
2022-08-10 01:31:08 -0400

    Clean up results to only have name and rating

--- search_api.py
+++ search_api.py
@@ -1,3 +1,4 @@
+from lib2to3.pytree import convert
 import requests
 import os
 import json
@@ -5,6 +6,24 @@ import json
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
+def clean_results(response):
+    # assign json results to variable
+    api_json = response.text
+
+    # convert json object to Python dictionary
+    api_dict = json.loads(api_json)
+
+    # store value of "results" key i.e. a list of the restaurants, inside a variable
+    restaurants = api_dict["results"]
+
+    # delete all keys in every element except the key "name"
+    for i in restaurants:
+        for k in list(i):
+            if k != "name" and k != "rating":
+                del i[k]
+        print(i)
 
 
 def search(name):
@@ -20,8 +39,7 @@ def search(name):
 
     response = requests.request("GET", url, headers=headers, data=payload)
 
-    info = response.text
-    print(info)
+    clean_results(response)
 
 
 search("mexicanrestaurants")
